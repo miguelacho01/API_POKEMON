87bcb80b18da271f06589e57d7954209665f9abc
Miguel Carvajal <miguelcarvajal7.mc@gmail.com>
2022-10-12 20:39:57 -0500

    ya casi

--- main.js
+++ main.js
@@ -21,37 +21,72 @@
 // }
 // cargarpokemon()
 
-const input = document.querySelector('.buscar')
-const boton = document.querySelector('.boton')
-const informacion = document.querySelector('.informacion')
+// const input = document.querySelector('.buscar')
+// const boton = document.querySelector('.boton')
+// const informacion = document.querySelector('.informacion')
 
-boton.addEventListener('click',(e)=>{
-    e.preventDefault
-    cargarpokemon(input.value)
+// boton.addEventListener('click',(e)=>{
+//     e.preventDefault
+//     cargarpokemon(input.value)
+// })
+
+// const cargarpokemon = async (pokemon)=>{
+//             const  respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+//             console.log(respuesta)
+//             datos = await respuesta.json()
+//            console.log(datos)
+//            crearPokemon(datos)
+
+//         }
+
+
+//         function crearPokemon(pokemon){
+//             const img = document.createElement('img')
+//             img.src = pokemon.sprites.front_default
+
+//             const h2 = document.createElement('h2')
+//             h2.textContent = pokemon.name
+
+//             const stats = document.createElement('p')
+//             p.textContent = pokemon.stats[0].stat.name
+//             p.appendChild(p)
+
+//             const div = document.createElement('div')
+//             div.appendChild(h2)
+//             div.appendChild(img)
+//             informacion.appendChild(div)
+
+
+const api = 'https://pokeapi.co/api/v2/pokemon/'
+const img = document.querySelector('#img')
+const abilities = document.querySelector('#abilities')
+
+
+
+async function buscarPokemon() {
+    const nombre = document.querySelector('.buscar').value
+    const respuesta = await fetch(api + nombre)
+    const datos = await respuesta.json()
+    console.log(datos)
+
+    
+    for(i=0;i<datos.abilities.length;i++){
+        habilidades=`${datos.abilities[i].ability.name}`
+    }
+    
+    img.innerHTML = `<img src="${datos.sprites.front_default}" ><p><b>Nombre: </b>${datos.name}</p>
+    <p><b>Habilidades:</b>${habilidades}</p>
+    <p> <b>Experiencia base:</b> ${datos.base_experience}</p>
+    <p><b>Movimiento:</b> ${datos.moves[0].move.name}</p>
+    
+`
+
+}
+const boton = document.querySelector('.boton')
+boton.addEventListener('click', (e) => {
+    e.preventDefault()
+    buscarPokemon()
 })
 
-const cargarpokemon = async (pokemon)=>{
-            const  respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-            console.log(respuesta)
-            datos = await respuesta.json()
-           console.log(datos)
-           crearPokemon(datos)
-           
-        }
-        
 
-        function crearPokemon(pokemon){
-            const img = document.createElement('img')
-            img.src = pokemon.sprites.front_default
 
-            const h2 = document.createElement('h2')
-            h2.textContent = pokemon.name
-
-            const div = document.createElement('div')
-            div.appendChild(img)
-            div.appendChild(h2)
-
-            informacion.appendChild(div)
-
-        }
-        
